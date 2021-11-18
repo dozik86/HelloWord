782a10b067675444f48160126182dff08c2af953
dezox <dozik86@gmail.com>
2021-11-18 23:48:40 +0100

    Initial Commit

---
+++ Shared/HelloWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWordApp.swift
+//  Shared
+//
+//  Created by Eugene on 18/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct HelloWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
